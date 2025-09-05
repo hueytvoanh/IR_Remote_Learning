641d7c8d70b4487a80c00db8cf8bc24022a9c16d
hueytvoanh <toan.daothanh@gmail.com>
2025-09-05 13:38:46 +0700

    Create SendTest.ino

---
+++ SendTest.ino
@@ -0,0 +1,31 @@
+#include <IRremote.h>
+
+IRsend irsend;
+
+void setup() {
+  // Initialize IR sender
+  irsend.begin();
+}
+
+void loop() {
+  // Raw timing data in microseconds
+  uint16_t rawData[] = {
+    3150, 9770, 480, 1570, 530, 520, 480, 520, 480, 520,
+    530, 1570, 430, 570, 530, 520, 530, 520, 480, 520,
+    480, 520, 530, 520, 480, 520, 480, 1620, 430, 570,
+    480, 520, 480, 570, 480, 1570, 480, 1570, 480, 1570,
+    480, 1570, 480, 520, 530, 1570, 430, 570, 480, 1570,
+    530, 1520, 530, 1570, 430, 570, 480, 520, 480
+  };
+  const int dataSize = sizeof(rawData) / sizeof(rawData[0]);
+
+  // Send the raw data with 38kHz frequency
+  irsend.sendPulseDistanceWidth(
+    38000, // Carrier frequency in Hz
+    rawData, 
+    dataSize,
+    0 // Number of repeats
+  );
+
+  delay(5000); // Wait 5 seconds before sending again
+}
