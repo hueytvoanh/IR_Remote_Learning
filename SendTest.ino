2202c7fe5b8845bb4c10d0352c41a2b5badb06b1
hueytvoanh <toan.daothanh@gmail.com>
2025-09-05 13:41:36 +0700

    Update SendTest.ino

--- SendTest.ino
+++ SendTest.ino
@@ -29,3 +29,31 @@ void loop() {
 
   delay(5000); // Wait 5 seconds before sending again
 }
+
+
+/*
+#include <IRremote.h>
+
+IRsend irsend;
+
+void setup() {
+  irsend.begin();
+}
+
+void loop() {
+  uint16_t rawData[] = {
+    3150, 9770, 480, 1570, 530, 520, 480, 520, 480, 520,
+    530, 1570, 430, 570, 530, 520, 530, 520, 480, 520,
+    480, 520, 530, 520, 480, 520, 480, 1620, 430, 570,
+    480, 520, 480, 570, 480, 1570, 480, 1570, 480, 1570,
+    480, 1570, 480, 520, 530, 1570, 430, 570, 480, 1570,
+    530, 1520, 530, 1570, 430, 570, 480, 520, 480
+  };
+
+  const int dataSize = sizeof(rawData) / sizeof(rawData[0]);
+
+  irsend.sendPulseDistanceWidth(38000, rawData, dataSize, 0);
+  
+  delay(10000); // Wait before re-sending
+}
+*/
