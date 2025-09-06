78022d703f1ec9db8825d92fa4cc59a296219dab
hueytvoanh <toan.daothanh@gmail.com>
2025-09-06 11:41:38 +0700

    Update Arduino_Record_Long_AirConditioner_Infrared_Signals.ino

--- Arduino_Record_Long_AirConditioner_Infrared_Signals/Arduino_Record_Long_AirConditioner_Infrared_Signals.ino
+++ Arduino_Record_Long_AirConditioner_Infrared_Signals/Arduino_Record_Long_AirConditioner_Infrared_Signals.ino
@@ -1,46 +1,62 @@
+Skip to content
+ 
+Search Gists
+Search...
+All gists
+Back to GitHub
+@chaeplin
+chaeplin/Arduino_Record_Long_AirConditioner_Infrared_Signals_10.ino
+Last active 7 years ago • Report abuse
+Code
+Revisions
+4
+Stars
+3
+Clone this repository at &lt;script src=&quot;https://gist.github.com/chaeplin/a3a4b4b6b887c663bfe8.js&quot;&gt;&lt;/script&gt;
+<script src="https://gist.github.com/chaeplin/a3a4b4b6b887c663bfe8.js"></script>
+Arduino_Record_Long_AirConditioner_Infrared_Signals_10
+Arduino_Record_Long_AirConditioner_Infrared_Signals_10.ino
+// http://www.analysir.com/blog/2014/03/19/air-conditioners-problems-recording-long-infrared-remote-control-signals-arduino/
+// http://www.analysir.com/blog/wp-content/uploads/2014/03/Arduino_Record_Long_AirConditioner_Infrared_Signals_10.txt
+// For LG AC
+
 /*
 Author: AnalysIR
-Revision: 1.0 - Initial release
-Revision: 1.1 - update generic digitalPinToInterrupt to support most arduino platform
-
+Revision: 1.0
 This code is provided to overcome an issue with Arduino IR libraries
-It allows you to capture raw timings for signals longer than 255 marks & spaces.
+It allows you to capture raw timings for signals longer than 255 marks &amp; spaces.
 Typical use case is for long Air conditioner signals.
-
 You can use the output to plug back into IRremote, to resend the signal.
-
 This Software was written by AnalysIR.
-
 Usage: Free to use, subject to conditions posted on blog below.
 Please credit AnalysIR and provide a link to our website/blog, where possible.
-
-Copyright AnalysIR 2014-2019
-
+Copyright AnalysIR 2014
 Please refer to the blog posting for conditions associated with use.
 http://www.analysir.com/blog/2014/03/19/air-conditioners-problems-recording-long-infrared-remote-control-signals-arduino/
-
 Connections:
 IR Receiver      Arduino
-V+          ->  +5v
-GND          ->  GND
-Signal Out   ->  Digital Pin 2
-(If using a 3V3 Arduino, you should connect V+ to +3V3)
-
-Tested on UNO only
+V+          -&gt;  +5v
+GND          -&gt;  GND
+Signal Out   -&gt;  Digital Pin 2
+(If using a 3V Arduino, you may connect V+ to +3V)
 */
 
 #define LEDPIN 13
 //you may increase this value on Arduinos with greater than 2k SRAM
 #define maxLen 800
-#define rxPinIR 2 //pin D2 or D3 on standard arduinos. (other pins may be available on More mordern modules like MEga2560, DUE, ESP8266, ESP32)
-
 
 volatile  unsigned int irBuffer[maxLen]; //stores timings - volatile because changed by ISR
 volatile unsigned int x = 0; //Pointer thru irBuffer - volatile because changed by ISR
 
 void setup() {
-  Serial.begin(115200); //change BAUD rate as required
-  attachInterrupt(digitalPinToInterrupt(rxPinIR), rxIR_Interrupt_Handler, CHANGE);//set up ISR for receiving IR signal
+  Serial.begin(38400); //change BAUD rate as required
+
+  pinMode(4, OUTPUT); // VCC pin
+  pinMode(3, OUTPUT); // GND ping
+  digitalWrite(4, HIGH); // VCC +5V mode
+  digitalWrite(3, LOW);  // GND mode
+
+  attachInterrupt(0, rxIR_Interrupt_Handler, CHANGE);//set up ISR for receiving IR signal
 }
 
 void loop() {
@@ -54,17 +70,33 @@ void loop() {
     Serial.print(F("Raw: (")); //dump raw header format - for library
     Serial.print((x - 1));
     Serial.print(F(") "));
-    detachInterrupt(digitalPinToInterrupt(rxPinIR));//stop interrupts & capture until finshed here
+    detachInterrupt(0);//stop interrupts &amp; capture until finshed here
     for (int i = 1; i < x; i++) { //now dump the times
       if (!(i & 0x1)) Serial.print(F("-"));
       Serial.print(irBuffer[i] - irBuffer[i - 1]);
       Serial.print(F(", "));
     }
+    Serial.println();
+    
+    // for bin output by chaeplin@gmail.com
+    for (int n = 3; n < x; n++) { //now dump the times
+      if (!(n & 0x1)) {
+        if ( (irBuffer[n] - irBuffer[n - 1]) > 1000 ) {
+          Serial.print("1");
+        } else {
+          Serial.print("0");
+        }
+        if ( ((n -2) % 8 ) == 0 ) {
+          Serial.print(" ");
+        }
+      }
+    }
+
     x = 0;
     Serial.println();
     Serial.println();
     digitalWrite(LEDPIN, LOW);//end of visual indicator, for this time
-    attachInterrupt(digitalPinToInterrupt(rxPinIR), rxIR_Interrupt_Handler, CHANGE);//re-enable ISR for receiving IR signal
+    attachInterrupt(0, rxIR_Interrupt_Handler, CHANGE);//re-enable ISR for receiving IR signal
   }
 
 }
@@ -74,3 +106,20 @@ void rxIR_Interrupt_Handler() {
   irBuffer[x++] = micros(); //just continually record the time-stamp of signal transitions
 
 }
+@hueytvoanh
+Comment
+ 
+Leave a comment
+ 
+Footer
+© 2025 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+GitHub Community
+Docs
+Contact
+Manage cookies
+Do not share my personal information
