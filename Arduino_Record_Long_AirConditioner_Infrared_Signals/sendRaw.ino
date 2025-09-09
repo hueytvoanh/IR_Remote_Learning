7db8d4b259657a8a4bc474b1ec2f6fcbbc7f66ae
hueytvoanh <toan.daothanh@gmail.com>
2025-09-09 20:40:06 +0700

    Update sendRaw.ino

--- Arduino_Record_Long_AirConditioner_Infrared_Signals/sendRaw.ino
+++ Arduino_Record_Long_AirConditioner_Infrared_Signals/sendRaw.ino
@@ -1,44 +1,15 @@
 // https://github.com/shirriff/Arduino-IRremote
-#include <IRremote.h>
+//#include <IRremote.h>
+#include <IRremote.hpp>
 
-IRsend irsend;
-
+//IRsend irsend;
+//IrSender irsend;
 
 void setup()
 {
+  //irsend.begin(3);
+  IrSender.begin(3);
 }
-
-// on 
-// Raw: (59) 8000, -4000, 520, -1564, 524, -524, 520, -540, 500, -536, 500, -1592, 500, -528, 520, -532, 520, -548, 500, -532, 520, -548, 500, -532, 520, -548, 500, -532, 520, -552, 496, -540, 500, -524, 520, -1584, 500, -1564, 520, -540, 500, -536, 500, -548, 500, -532, 520, -1600, 500, -540, 500, -1576, 500, -1588, 520, -1576, 500, -540, 500, 
- 
- 
-// off
-// Raw: (59) 8000, -4000, 524, -1544, 552, -496, 548, -512, 524, -500, 548, -1556, 528, -520, 524, -500, 548, -520, 528, -1532, 552, -1548, 524, -516, 524, -496, 552, -520, 524, -512, 528, -512, 524, -508, 548, -496, 552, -504, 548, -504, 552, -516, 504, -528, 552, -1572, 500, -536, 504, -1572, 500, -540, 500, -540, 496, -548, 520, -1540, 552, 
-
-// on
-unsigned int sendbuf1[] = { 
-8000, 4000, 520, 1564, 524, 524, 520, 540, 500, 536, 500, 1592, 500, 528, 520, 532, 520, 548, 500, 532, 520, 548, 500, 532, 520, 548, 500, 532, 520, 552, 496, 540, 500, 524, 520, 1584, 500, 1564, 520, 540, 500, 536, 500, 548, 500, 532, 520, 1600, 500, 540, 500, 1576, 500, 1588, 520, 1576, 500, 540, 500};
-
-// off
-unsigned int sendbuf2[] = { 
-8000, 4000, 524, 1544, 552, 496, 548, 512, 524, 500, 548, 1556, 528, 520, 524, 500, 548, 520, 528, 1532, 552, 1548, 524, 516, 524, 496, 552, 520, 524, 512, 528, 512, 524, 508, 548, 496, 552, 504, 548, 504, 552, 516, 504, 528, 552, 1572, 500, 536, 504, 1572, 500, 540, 500, 540, 496, 548, 520, 1540, 552};
-
-  unsigned int rawData[] = {
-    3168, 9856, 512, 1592, 492, 528, 536, 516, 484, 548, 520, 1548,
-    488, 552, 516, 516, 512, 520, 516, 1552, 516, 1556, 488, 552,
-    484, 548, 488, 548, 484, 544, 520, 516, 484, 548, 484, 548,
-    512, 528, 516, 524, 516, 520, 484, 548, 484, 1588, 484, 548,
-    488, 1588, 508, 532, 484, 556, 512, 520, 484, 1592, 512
-  }; 
-
- unsigned int rawData1[] = {
-    3168, 9856, 512, 1592, 492, 528, 536, 516, 484, 548, 520, 1548,
-    488, 552, 516, 516, 512, 520, 516, 1552, 516, 1556, 488, 552,
-    484, 548, 488, 548, 484, 544, 520, 516, 484, 548, 484, 548,
-    512, 528, 516, 524, 516, 520, 484, 548, 484, 1588, 484, 548,
-    488, 1588, 508, 532, 484, 556, 512, 520, 484, 1592, 512
-  };
-
 unsigned int rawData_ON[] = {
   3368, 9848, 500, 1584, 504, 528, 504, 528, 504, 532, 500, 1560,
   504, 540, 500, 532, 500, 532, 500, 532, 500, 532, 500, 532,
@@ -48,34 +19,25 @@ unsigned int rawData_ON[] = {
 };
 
 unsigned int rawData_OFF[] = {
-  3152, 9860, 528, 1576, 500, 536, 504, 528, 504, 532, 500, 1564,
-  500, 540, 500, 532, 500, 532, 500, 1564, 500, 1572, 500, 540,
-  504, 528, 500, 532, 500, 532, 504, 528, 504, 528, 504, 528,
-  504, 540, 500, 536, 504, 528, 504, 528, 504, 1572, 500, 532,
-  500, 1572, 500, 540, 500, 540, 500, 532, 500, 1572
+  3168, 9856, 512, 1592, 492, 528, 536, 516, 484, 548, 520, 1548,
+    488, 552, 516, 516, 512, 520, 516, 1552, 516, 1556, 488, 552,
+    484, 548, 488, 548, 484, 544, 520, 516, 484, 548, 484, 548,
+    512, 528, 516, 524, 516, 520, 484, 548, 484, 1588, 484, 548,
+    488, 1588, 508, 532, 484, 556, 512, 520, 484, 1592, 512
 };
  
 void loop() {
- /*
-  testRaw("RAW1", sendbuf1, sizeof(sendbuf1)/sizeof(int));
-  delay(5000);
-  testRaw("RAW2", sendbuf2, sizeof(sendbuf2)/sizeof(int));
-  delay(5000);
-  */
-   //testRaw("rawData", rawData, sizeof(rawData)/sizeof(int));
+ IrSender.sendRaw(rawData_ON, sizeof(rawData_ON) / sizeof(rawData_ON[0]), NEC_KHZ); // Note the approach used to automatically calculate the size of the array.
 
-   //IrSender.sendRaw(rawData1, sizeof(rawData1) / sizeof(rawData1[0]), NEC_KHZ); // Note the approach used to automatically calculate the size of the array.
- irsend.sendRaw(rawData_ON, sizeof(rawData_ON) / sizeof(rawData_ON[0]), NEC_KHZ); // Note the approach used to automatically calculate the size of the array.
-
-    delay(20000); // delay must be greater than 8 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
+    delay(10000); // delay must be greater than 8 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
 
  
- irsend.sendRaw(rawData_OFF, sizeof(rawData_OFF) / sizeof(rawData_OFF[0]), NEC_KHZ); // Note the approach used to automatically calculate the size of the array.
+    IrSender.sendRaw(rawData_OFF, sizeof(rawData_OFF) / sizeof(rawData_OFF[0]), NEC_KHZ); // Note the approach used to automatically calculate the size of the array.
 
-    delay(20000); // delay must be greater than 8 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
+    delay(10000); // delay must be greater than 8 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
 }
 
 void testRaw(char *label, unsigned int *rawbuf, int rawlen) {
-    irsend.sendRaw(rawbuf, rawlen, 38 /* kHz */);
+    IrSender.sendRaw(rawbuf, rawlen, 38 /* kHz */);
     delay(200);
 }   
