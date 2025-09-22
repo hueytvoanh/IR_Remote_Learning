0aef1508d37c36b0a3ed5725786043b65b9ced58
hueytvoanh <toan.daothanh@gmail.com>
2025-09-22 16:10:30 +0700

    Update SendIRCustom.ino

--- SendIRCustom.ino
+++ SendIRCustom.ino
@@ -1,45 +1,47 @@
-const int irPin = 9; // Set your IR LED pin here
+const int IR_LED_PIN = 3;  // Change to your IR LED pin
+
+// Timing constants in microseconds
+const unsigned int HEADER_PULSE = 3180;
+const unsigned int HEADER_SPACE = 9880;
+
+const unsigned int BIT_PULSE = 560;
+const unsigned int ONE_SPACE = 1572;
+const unsigned int ZERO_SPACE = 532;
 
 void setup() {
-  pinMode(irPin, OUTPUT);
-  // Call your send function here
-  sendCustomIRData(0xA0200001);
+  pinMode(IR_LED_PIN, OUTPUT);
+  digitalWrite(IR_LED_PIN, LOW);
 }
 
 void loop() {
-  // Repeat or add delay as needed
+  sendCustomIr(0x88C0051);
+  delay(5000); // Wait 5 seconds before sending again
 }
 
-// Function to send your custom IR data matching your raw data timings
-void sendCustomIRData(uint32_t data) {
-  // Send header (leader) mark
-  sendPulse(3180);
-  sendSpace(9928);
-  
-  // Send 32 bits
-  for (int i = 0; i < 32; i++) {
-    sendPulse(560); // pulse for each bit
-    if (data & 0x1) {
-      sendSpace(1690); // bit '1'
+void sendPulse(unsigned int duration) {
+  digitalWrite(IR_LED_PIN, HIGH);
+  delayMicroseconds(duration);
+  digitalWrite(IR_LED_PIN, LOW);
+  delayMicroseconds(560); // Gap between pulses, typical at 560us
+}
+
+void sendSpace(unsigned int duration) {
+  digitalWrite(IR_LED_PIN, LOW);
+  delayMicroseconds(duration);
+}
+
+void sendCustomIr(unsigned long code) {
+  // Send header
+  sendPulse(HEADER_PULSE);
+  sendSpace(HEADER_SPACE);
+
+  // Send 24 bits of data (assuming 24-bit code)
+  for (int i = 23; i >= 0; i--) {
+    sendPulse(BIT_PULSE);
+    if (code & (1UL << i)) {
+      sendSpace(ONE_SPACE); // Bit '1'
     } else {
-      sendSpace(560);  // bit '0'
+      sendSpace(ZERO_SPACE); // Bit '0'
     }
-    data >>= 1; // shift to next bit
   }
-
-  // Send final pulse
-  sendPulse(560);
-  // Optional: add a delay here if needed
-}
-
-// Helper functions to send pulse and space
-void sendPulse(int microseconds) {
-  digitalWrite(irPin, HIGH);
-  delayMicroseconds(microseconds);
-  digitalWrite(irPin, LOW);
-}
-
-void sendSpace(int microseconds) {
-  digitalWrite(irPin, LOW);
-  delayMicroseconds(microseconds);
 }
